ae4f19f6e1327d8eb43864e73ea021f1448543a6
Abdullah Md. ShahPoran <abdullahshahporan@gmail.com>
2024-08-14 08:55:15 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/shah/assignment_m_10/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shah.assignment_m_10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(IntroApp());
+}
+class IntroApp extends StatelessWidget {
+  const IntroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      home: Home(),
+      title: 'Intro App',
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('Greeting App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget> [
+            Text('Hello World!',
+              style:TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ) ,
+            ),
+            SizedBox(height: 10,),
+            Text('Welcome to Flutter!',
+                style: TextStyle(fontSize: 20)),
+            SizedBox(height: 10,),
+            Image.asset('assets/image/flutter.png',width: 200,height: 200,),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: () {
+                final snackBar = SnackBar(
+                  content: Text('Button Pressed!'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              ),
+              child: Text('Press Me'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
